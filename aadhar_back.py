293a9e102e88559304a2bb862f14f60bc1be0063
saujanya01 <saujanyatiwari455@gmail.com>
2019-06-16 21:20:09 +0530

    First and final

---
+++ aadhar_back.py
@@ -0,0 +1,39 @@
+import cv2
+import pytesseract
+import os
+import re
+
+def aadhar_b(img):
+    
+    # file = os.listdir('./data/aadhar')
+    # for i in file:
+    #     x=x.split('.')
+    #     if x[0].endswith('back')
+    #     filename = i
+    #img = cv2.imread('./data/aadhar/'+filename)
+    #cv2.imshow('Original',img)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    img = gray
+    #cv2.imshow("grayscale",img)
+    
+    (h,w) = img.shape
+    st = pytesseract.image_to_string(img[:,w//2:w])
+    #s = st.split('\n')
+    #st = pytesseract.image_to_string(img)
+    addpos=re.split("Address:",st)
+    addi=addpos[1]
+    addend=re.split(r'([0-9]{6})',addi)
+    ''.join(addend)
+    address=addend[0]+addend[1]
+    return {'address':address}
+    '''
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()'''
+# img = cv2.imread('adback.jpeg')
+# s = aadhar_back(img)
+# addpos=re.split("Address:",s)
+# addi=addpos[1]
+# addend=re.split(r'([0-9]{6})',addi)
+# ''.join(addend)
+# address=addend[0]+addend[1]
+# print(s)
---
+++ aadhar_front.py
@@ -0,0 +1,30 @@
+import cv2
+import pytesseract
+import re
+
+def aadhar_f(img):
+    #img = cv2.imread('./data/pan.jpeg')
+    #cv2.imshow('Original',img)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    img = gray
+    #cv2.imshow("grayscale",img)
+    
+    (h,w) = img.shape
+    #st = pytesseract.image_to_string(img[:,w//2:w])
+    st = pytesseract.image_to_string(img)
+    #dob=re.findall(r'(((?:0[0-9])|(?:[1-2][0-9])|(?:3[0-1]))\/((?:0[1-9])|(?:1[0-2]))\/(\d{4}))',st)
+    dob=re.findall(r'([0-9]{4})',st)
+    dob=dob[0]
+    aad=re.findall(r'([0-9]{4}\s[0-9]{4}\s[0-9]{4})',st)
+    return {'dob':dob,'a_no':aad}
+    
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
+    
+# img = cv2.imread('adfront.jpg')
+# s = aadhar_front(img)
+# dob=re.findall(r'(((?:0[0-9])|(?:[1-2][0-9])|(?:3[0-1]))\/((?:0[1-9])|(?:1[0-2]))\/(\d{4}))',s)
+# aad=re.findall(r'([0-9]{4}\s[0-9]{4}\s[0-9]{4})',s)
+
+
+# print(s)
